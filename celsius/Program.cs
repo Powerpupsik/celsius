435b9c02d26331c51b0512fdfc13174bf60f30e4
Powerpupsik <raima.levistu@gmail.com>
2019-10-23 17:00:29 +0300

    celsius

---
+++ celsius/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+
+            double celsius = 79;
+            Console.WriteLine("Celsius: " + celsius);
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+        
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine("Celsius: " + celsius);
+
+            Console.ReadLine();
+        }
+    }
+}
